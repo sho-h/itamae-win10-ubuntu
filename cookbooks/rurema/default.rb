7328c8da4a1ecb81a72665419f6b39a257087e61
Sho Hashimoto <sho-h@netlab.jp>
2017-05-04 13:38:16 +0900

    るりま用のレシピで以下の修正。 * レポジトリをcloneするように修正(ただし、known_hostsが必要) * RAMをマウントするように修正

--- cookbooks/rurema/default.rb
+++ cookbooks/rurema/default.rb
@@ -4,14 +4,36 @@ execute 'create doc directory' do
   user node['target_user']
 end
 
-=begin
 # TODO: gitリソースでブランチが指定できる方法がわかれば変更する。
 %w(doctree bitclust).each do |repos|
+  repos_dir = File.join(doc_dir, repos)
   execute "clone rurema #{repos}" do
-    command "git clone git@github.com:rurema/#{repos}.git git"
+    command "git clone git@github.com:sho-h/#{repos}.git"
     user node['target_user']
     cwd doc_dir
-    not_if "ls #{doc_dir}/#{repos}"
+    not_if "ls #{repos_dir}"
+  end
+
+  execute "add #{repos} upstream" do
+    command "git remote add upstream git@github.com:rurema/#{repos}.git"
+    user node['target_user']
+    cwd repos_dir
+    not_if "git remote -v | grep upstream"
   end
 end
-=end
+
+# bitclustのDB用の領域をRAMに確保。
+bitclust_dir = File.join(doc_dir, 'bitclust')
+db_dir = File.join(bitclust_dir, 'db')
+execute "create bitclust db directory" do
+  command "mkdir #{db_dir}"
+  user node['target_user']
+  cwd bitclust_dir
+  not_if "ls | grep db"
+end
+
+execute "mount db directory" do
+  command "mount -t tmpfs -o size=128M tmpfs #{db_dir}"
+  user 'root'
+  not_if "mount | grep tmpfs | grep bitclust/db"
+end
